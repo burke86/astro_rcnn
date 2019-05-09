112e8826de329ae5f439193a6e51e95619fec0b7
Colin Burke <colin@wirelessprv-10-192-175-74.near.illinois.edu>
2019-05-09 15:47:20 -0500

    generate training masks

--- phosim_release/gen_training_cat.py
+++ phosim_release/gen_training_cat.py
@@ -1,21 +1,27 @@
 import numpy as np
 import subprocess
 
-# spiral galaxy
+def make_star():
+    box = 2.0 # arceconds
+    ra = np.random.normal(-box/3600,box/3600) # deg
+    dec = np.random.normal(-box/3600,box/3600) # deg
+    mag = np.random.normal(22.0,1)
+    star = ('object 0 %f %f %f ../sky/sed_flat.txt 0 0 0 0 0 0 star none none \n' % (ra,dec,mag))
+    return star
 
-def make_spiral():
+def make_gal():
     # disk
-    box = 2 # arceconds
+    box = 2.0 # arceconds
     ra = np.random.normal(-box/3600,box/3600) # deg
     dec = np.random.normal(-box/3600,box/3600) # deg
     mag = np.random.normal(21.0,1)
-    ax1 = np.clip(np.random.normal(3,2),0,8) # size of axis 1 in arcseconds
+    ax1 = np.clip(np.random.normal(2,.5),0,8) # size of axis 1 in arcseconds
     ax2 = ax1 # size of axis 2 in arcseconds
     ax3 = ax1/8 # size of axis 3 in arcseconds
     phi = np.random.uniform(0,360) # polar angle in degrees
     theta = np.random.uniform(0,360) # position angle in degrees
     n = np.clip(np.random.normal(1,0.1),.1,4) # Sersic index
-    fclump = np.clip(np.random.normal(.2,.08),0,.4) # fraction of light in clumps
+    fclump = np.clip(np.random.normal(.1,.08),0,.4) # fraction of light in clumps
     nclump = np.clip(np.random.normal(50,20),0,1000) # number of clumps
     rclump = np.clip(np.random.normal(0.3,0.1),0,1) # gaussian clump size in arcseconds
     fspiral = np.clip(np.random.normal(0.5,0.2),0,0.8) # fraction of light in spiral
@@ -44,19 +50,61 @@ def make_spiral():
     thetaspiral = 0 # position angle of spiral
     pars = (ra,dec,mag,ax1,ax2,ax3,phi,theta,n,fclump,nclump,rclump,fspiral,waspiral,rbar,rspiral,thetaspiral)
     bulge = ('object 0 %f %f %f ../sky/sed_flat.txt  0 0 0 0 0 0 sersicComplex %f %f %f %f %f %f %f %f %f %f %f %f %f %f none none \n' % pars)
-    return header+disk+bulge
+    return disk+bulge
     # elliptical
-for i in range(0,10):
+for i in range(0,1):
     header = ('rightascension 0\ndeclination 0\nazimuth 0\naltitude 89\nrottelpos 0\nfilter 2\nseeing 0.67\nvistime 90.0\nnsnap 1\n')
-    name = './examples/spiral%d' % i
+    name = './examples/img%d' % i
+    gal1 = make_gal()
+    gal2 = make_gal()
+    gal3 = make_gal()
+    star1 = make_star()
+    # make image:
+    make_third = False
     with open(name,'w+') as f:
         f.write(header)
-        f.write(make_spiral())
+        f.write(star1)
+        f.write(gal1)
+        f.write(gal2)
         # make second galaxy 20 % of the time
-        if np.random.uniform() > 0.2:
-            f.write(make_spiral())
-            # make second galaxy 10 % of the time
-            if np.random.uniform() > 0.5:
-                f.write(make_spiral())
-    bashCommand = './phosim %s -c examples/training -i decam --ds9' % name
+        if np.random.uniform() > 0.8:
+            f.write(gal3)
+            make_third = True
+    bashCommand = './phosim %s -c examples/training -i decam' % name
     subprocess.call(bashCommand.split())
+    bashCommand = 'mv ./output/decam_e_9999_f2_chip_E000.fits.gz ./output/training_img%d.fits.gz' % i
+    subprocess.call(bashCommand.split())
+    # make masks:
+    name = './examples/star%d_0' % i
+    with open(name,'w+') as f:
+        f.write(header)
+        f.write(star1)
+    bashCommand = './phosim %s -c examples/training_nobg -i decam' % name
+    subprocess.call(bashCommand.split())
+    bashCommand = 'mv ./output/decam_e_9999_f2_chip_E000.fits.gz ./output/training_star%d_0.fits.gz' % i
+    subprocess.call(bashCommand.split())
+    name = './examples/gal%d_1' % i
+    with open(name,'w+') as f:
+        f.write(header)
+        f.write(gal1)
+    bashCommand = './phosim %s -c examples/training_nobg -i decam' % name
+    subprocess.call(bashCommand.split())
+    bashCommand = 'mv ./output/decam_e_9999_f2_chip_E000.fits.gz ./output/training_gal%d_1.fits.gz' % i
+    subprocess.call(bashCommand.split())
+    name = './examples/gal%d_2' % i
+    with open(name,'w+') as f:
+        f.write(header)
+        f.write(gal2)
+    bashCommand = './phosim %s -c examples/training_nobg -i decam' % name
+    subprocess.call(bashCommand.split())
+    bashCommand = 'mv ./output/decam_e_9999_f2_chip_E000.fits.gz ./output/training_gal%d_2.fits.gz' % i
+    subprocess.call(bashCommand.split())
+    if make_third == True:
+        name = './examples/gal%d_3' % i
+        with open(name,'w+') as f:
+            f.write(header)
+            f.write(gal3)
+        bashCommand = './phosim %s -c examples/training_nobg -i decam' % name
+        subprocess.call(bashCommand.split())
+        bashCommand = 'mv ./output/decam_e_9999_f2_chip_E000.fits.gz ./output/training_gal%d_3.fits.gz' % i
+        subprocess.call(bashCommand.split())
