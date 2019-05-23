1579e9d554011ef0e76f4e481f58b66c23f31f87
Colin Burke <cburke@des30.fnal.gov>
2019-05-23 14:06:27 -0500

    cleanup

--- simulate.py
+++ simulate.py
@@ -1,9 +1,12 @@
 import os
 import time
 import subprocess
+import multiprocessing as mp
 from multiprocessing.dummy import Pool as ThreadPool
 
 os.chdir("./phosim_release")
+
+TRAIN_DIR = "/data/des80.a/data/cburke/trainingset"
 
 def bash(command,print_out=True):
     if print_out: print(command)
@@ -12,12 +15,13 @@ def bash(command,print_out=True):
 
 class PhoSimSet:
 
-    def __init__(self,set_num):
+    def __init__(self,set_num,train_dir="../trainingset/"):
         self.set = set_num
+        self.seed = 1000 + set_num
+        self.train_dir = train_dir
 
     def mask(self,line):
-        OUT_DIR = os.path.abspath("../trainingset/")
-        out_dir = os.path.abspath(os.path.join(OUT_DIR,"set_%d/" % self.set))
+        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.set))
         if len(line.strip()) != 0:
             i = line.split()[1].split('.')[0]
             if line.split()[-3] == "star":
@@ -26,13 +30,11 @@ class PhoSimSet:
                 obj_class = "gal"
             # Make a new catalog for each source in the image
             with open("./examples/obj%s" % i,"w+") as fi:
-                fi.write("rightascension 0\ndeclination 0\nfilter 2\nvistime 90.0\nnsnap 1\nobshistid %s\n" % i)
+                fi.write("filter 2\nvistime 90.0\nnsnap 1\nobshistid %s\nseed %d\n" % (i,self.seed))
                 fi.write(line)
                 fi.write("\n")
-            print(i)
-            print(line)
             # Now run PhoSim with this single object and no background to make mask
-            """bash("./phosim examples/obj%s -c examples/training_nobg -i decam -t 1 -e 0" % i)
+            bash("./phosim examples/obj%s -c examples/training_nobg -i decam -t 1 -e 0" % i)
             out_to = os.path.abspath(os.path.join(out_dir,"%s%s.fits.gz" % (obj_class,i)))
             out_from = "./output/decam_e_%s_f2_4S_E000.fits.gz" % i
             wait = 0
@@ -45,14 +47,23 @@ class PhoSimSet:
                 bash("mv %s %s" % (out_from, out_to))
                 os.remove("./examples/obj%s" % i)
             except: # off chip
-            """
+                pass
         return
 
     def img(self,band):
-        OUT_DIR = os.path.abspath("../trainingset/")
-        out_dir = os.path.abspath(os.path.join(OUT_DIR,"set_%d/" % self.set))
+        m0 = 15.0
+        m1 = 23.5
+        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.set))
         out_to = os.path.abspath(os.path.join(out_dir,"img_%s.fits.gz" % band))
-        bash("./phosim examples/maskrcnn_catalog_%s -c examples/training -i decam -t 42 -e 0" % band)
+        with open("examples/maskrcnn_catalog_%s" % band,"w+") as f:
+            if band == "g":
+                f.write("filter 1\nvistime 90.0\nnsnap 1\nobshistid 9999999\nseed %d\nstars %f %f 0.1\ngalaxies %f %f 0.1" % (self.seed,m0,m1,m0,m1))
+            elif band == "r":
+                f.write("filter 2\nvistime 90.0\nnsnap 1\nobshistid 9999998\nseed %d\nstars %f %f 0.1\ngalaxies %f %f 0.1" % (self.seed,m0,m1,m0,m1))
+            elif band == "i":
+                f.write("filter 3\nvistime 90.0\nnsnap 1\nobshistid 9999997\nseed %d\nstars %f %f 0.1\ngalaxies %f %f 0.1" % (self.seed,m0,m1,m0,m1))
+        # Run PhoSim
+        bash("./phosim examples/maskrcnn_catalog_%s -c examples/training -i decam -t %d -e 0" % (band,mp.cpu_count()//3))
         if band == 'g':
             bash("mv ./output/decam_e_9999999_f1_4S_E000.fits.gz %s" % out_to)
         elif band == 'r':
@@ -62,9 +73,7 @@ class PhoSimSet:
         return
 
     def simulate(self):
-        #os.chdir("./phosim_release")
-        OUT_DIR = os.path.abspath("../trainingset/")
-        out_dir = os.path.abspath(os.path.join(OUT_DIR,"set_%d/" % self.set))
+        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.set))
         if os.path.exists(out_dir):
             print("Error: Set %d already exists." % self.set)
             return
@@ -85,13 +94,11 @@ class PhoSimSet:
                     if ".1" in obj_id: # 1st component of galaxy
                         # add second component
                         lines.append(line+"\n"+readlines[i+2])
-                    if ".2" in obj_id:
+                    elif ".2" in obj_id:
                         continue # already added 2nd component
                     else: # star
                         lines.append(line)
-            print(lines)
-            return
-            pool = ThreadPool(120)
+            pool = ThreadPool(mp.cpu_count())
             out = pool.map(self.mask, lines)
             pool.close()
             pool.join()
@@ -101,6 +108,6 @@ class PhoSimSet:
             except: pass
 
 if __name__ == "__main__":
-    for i in range(2,100):
-        s = PhoSimSet(i)
+    for i in range(100):
+        s = PhoSimSet(i,TRAIN_DIR)
         s.simulate()
