d1f0829dc8ebb91bdcf3dc74fcfab5f8c4c8fa7d
Yu-Ching Chen <ycchen@descmp2.cosmology.illinois.edu>
2019-06-06 11:55:16 +0800

    add code for download.py

--- download_image.py
+++ download_image.py
@@ -9,13 +9,25 @@ The image will be named as 'Jxxxx+xxxx.fits' based on ra and dec
 
 def _make_name(ra,dec):
 
-    # generate the name for the objects (e.g. Jxxxx+xxxx)
+    # generate the name for the objects (e.g. Jxxxx+xxxx)    
 
-    ################
-    # Fill in code #
-    ################
+    ra = ra/15.
+    rah=int(ra)
+    ram=int(60*(ra-rah))
+    ras=3600.*(ra-rah-(ram/60.))
 
-    return name
+    if (dec >= 0.0):
+        decd=int(dec)
+        decm=int(60.*(dec-decd))
+        decs=3600.*(dec-decd-(decm/60.))
+        return "J%02d%02d%05.2f+%02d%02d%04.1f" % (rah,ram,ras,decd,decm,decs)
+    else:
+        decd=-int(dec)
+        dec=-dec
+        decm=int(60.*(dec-decd))
+        decs=3600.*(dec-decd-(decm/60.))
+        dec=-dec        
+        return "J%02d%02d%05.2f-%02d%02d%04.1f" % (rah,ram,ras,decd,decm,decs)
 
 
 def get_image(ra,dec,size):
