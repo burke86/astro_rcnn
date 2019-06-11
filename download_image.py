ab073efa6ba53a05e07754d1da13b36dcfdc1941
Yu-Ching Chen <ycchen@descmp2.cosmology.illinois.edu>
2019-06-11 12:55:58 -0500

    update download image scripts

--- download_image.py
+++ download_image.py
@@ -29,13 +29,21 @@ def _make_name(ra,dec):
         dec=-dec        
         return "J%02d%02d%05.2f-%02d%02d%04.1f" % (rah,ram,ras,decd,decm,decs)
 
+def get_multi_images(ra,dec,size,output_dir):
 
-def get_image(ra,dec,size):
+    make_dir(output_dir)
+    bands = ["g","r","z"]
+    for band in bands:
+        output_path = "%s/img_%s.fits" % (output_dir,band)
+        get_image(ra,dec,size,band,output_path)
+
+def get_image(ra,dec,size,band,output_path):
 
     # set up some values
+    '''
     band = "g"
     name = _make_name(float(ra),float(dec)) 
-
+    '''
     query = "wget 'http://legacysurvey.org/viewer/fits-cutout/?ra=%s&dec=%s&" % (ra,dec)+\
             "size=%s&layer=decals-dr7&pixscale=0.262&bands=%s' -O out.fits" % (size,band)
 
@@ -43,17 +51,35 @@ def get_image(ra,dec,size):
     os.system(query)
 
     # change the name
-    os.system("mv out.fits %s.fits" % name)
+    os.system("mv out.fits %s" % output_path)
+
+def check_set_number(testset_dir):
+
+    number = len(os.listdir(testset_dir))
+
+    return number
+
+def make_dir(dir_name):
+
+    if not os.path.exists(dir_name):
+        print("-- Making dir: %s" % dir_name)
+        os.system("mkdir %s" % dir_name)
     
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
+    testset_dir = "testset"
+
+    make_dir(testset_dir)
+
+    if len(sys.argv) != 4 :
         print("Incorrect number of parameters !!")
         print("Input Format:python download_image.py [ra(deg)] [dec(deg)] "+\
-              "[size(arcsec)]")
+              "[size(arcsec)] ")
+
     else:
         ra = sys.argv[1]
         dec = sys.argv[2]
         size = sys.argv[3]
-        get_image(ra,dec,size)
+        number = check_set_number(testset_dir)
+        get_multi_images(ra,dec,size,"%s/set_%s" %(testset_dir,number))
---
+++ download_image_abell_50.py
@@ -0,0 +1,23 @@
+# generate 50 512x512 image at one of the abell cluster
+# example: ACO 1656 : ra=194.9529 dec=27.9806
+
+from download_image import *
+
+if __name__ == "__main__":
+
+    ra_cen=194.9529
+    dec_cen=27.9806
+    shift_ra=512*0.27/3600
+    shift_dec=512*0.27/3600
+
+    output_dir = "testset"
+    make_dir(output_dir)
+
+    for n_ra in [-2,-1,0,1,2]:
+        for n_dec in [-4,-3,-2,-1,0,1,2,3,4,5]:
+            ra = ra_cen+n_ra*shift_ra
+            dec = dec_cen+n_dec*shift_dec 
+            size = 512
+            number = check_set_number(output_dir)
+            get_multi_images(ra,dec,size,"%s/set_%s" %(output_dir,number))
+
