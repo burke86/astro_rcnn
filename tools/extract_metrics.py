74c259caf3e8db66c2f7090d8d83d64c9d818a9e
Anshul <anshuls3@illinois.edu>
2020-06-19 17:37:47 -0500

    Histogram visualizations

--- tools/extract_metrics.py
+++ tools/extract_metrics.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from astropy.io import fits
+import matplotlib.pyplot as plt
 
 
 
@@ -10,6 +11,16 @@ def get_fits_intensity(fp):
         data = hdul[0].data
     intensity = np.sum(data)
     return intensity
+
+def intensity_hist(fp):
+    data = 0
+    with fits.open(fp) as hdul:
+        data = hdul[0].data
+    _ = plt.hist(data[0:256,0:256].flatten(), bins = "auto")
+    plt.show()
+    return None
+    
+
 
 
 
@@ -23,8 +34,9 @@ def get_fits_intensity(fp):
 
 
 if __name__ == "__main__":
-    ROOT_PATH = "../../example/set_0/"
-    print(get_fits_intensity(os.path.join(ROOT_PATH, 'img_r.fits')))
-    print(get_fits_intensity(os.path.join(ROOT_PATH, 'img_g.fits')))
-    print(get_fits_intensity(os.path.join(ROOT_PATH, 'img_z.fits')))
+    ROOT_PATH = "/Users/anshul/Desktop/astro_rcnn/hsc_test/set_0/"
+    print(intensity_hist(os.path.join(ROOT_PATH, 'img_g_l.fits')))
+    #print(intensity_hist(os.path.join(ROOT_PATH, 'img_r.fits')))
+    #print(intensity_hist(os.path.join(ROOT_PATH, 'img_g.fits')))
+    #print(intensity_hist(os.path.join(ROOT_PATH, 'img_z.fits')))
     pass
