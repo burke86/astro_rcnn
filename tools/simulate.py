08c982897c027b89888a63ab198a852cef4d6a20
burke86 <burke86@purdue.edu>
2020-07-08 22:45:31 -0500

    generalization IV for simulate.py

--- tools/simulate.py
+++ tools/simulate.py
@@ -2,6 +2,9 @@ import os
 import time
 import subprocess
 import multiprocessing as mp
+from glob import glob as glob
+from itertools import repeat
+from random import randrange
 import numpy as np
 from multiprocessing.dummy import Pool as ThreadPool
 from astropy.io import fits
@@ -13,6 +16,8 @@ COMMANDFILE_IMG = os.path.abspath("training")
 COMMANDFILE_MASK = os.path.abspath("training_nobg")
 os.chdir(PHOSIM_DIR)
 
+SEED = randrange(0,9999999)
+
 def bash(command,print_out=True):
     if print_out: print(command)
     try: return subprocess.call(command.split())
@@ -20,26 +25,31 @@ def bash(command,print_out=True):
 
 class PhoSimSet:
 
-    def __init__(self,set_num,instrument,bands,exptimes,maglim,train_dir,nproc):
+    def __init__(self,npointing,instrument,fov,bands,exptimes,maglim,train_dir,nproc):
+        self.npointing = npointing
         self.instrument = instrument
         self.bands = bands
         self.maglim = maglim
-        self.set = set_num
-        self.seed = 0 + set_num # Use a random scene each time
+        self.seed = SEED + npointing # Use a random scene each time
         self.train_dir = train_dir
         self.nproc = nproc
         # something roughly like DES foorprint as a test (avoiding galacitic plane)
         self.ra = np.random.uniform(0,60) # deg
         self.dec = np.random.uniform(-70,10) # deg
+        self.fov = fov # Degrees to simulate field of stars and galaxies 
         # Map each band to a filter number, obsID, and exposure time (WARNING: assuming phosim ugrizy=012345!)
-        bands_list = ['u','g','r','i','z','Y']
-        self.filterid = dict(zip(bands_list, [0,1,2,3,4,5]))
-        self.obsid = dict(zip(bands_list, [9999999,9999998,9999997,9999996,9999995,9999994]))
+        if self.instrument == 'subaru_hsc': # grizY
+            bands_list = ['g','r','i','z','Y']
+        else: # Assume ugriz (LSST, decam, etc.)
+            bands_list = ['u','g','r','i','z','Y']
+        self.filterid = dict(zip(bands_list, range(len(bands_list))))
+        self.obsid = dict(zip(bands_list, range(9999999,999999-len(bands_list),-1)))
         self.exptime = dict(zip(self.bands, exptimes))
 
-    def mask(self,line):
-        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.set))
+    def mask(self,line,chip,setnum):
+        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % setnum))
         if len(line.strip()) != 0:
+            # Object number from trimcatalog line gives us the obsID (and therefore the filename)
             i = line.split()[1].split('.')[0]
             if line.split()[-3] == "star":
                 obj_class = "star"
@@ -55,7 +65,7 @@ class PhoSimSet:
             # Now run PhoSim with this single object and no background to make mask
             bash("./phosim examples/obj%s -c %s -i %s -t %d -e 0" % (i,COMMANDFILE_MASK,self.instrument,int(np.sqrt(self.nproc))))
             out_to = os.path.abspath(os.path.join(out_dir,"%s%s.fits.gz" % (obj_class,i)))
-            out_from = "./output/%s_e_%s_f%d_4S_E000.fits.gz" % (self.instrument,i,self.filterid[band])
+            out_from = "./output/%s_e_%s_f%d_%s_E000.fits.gz" % (self.instrument,i,self.filterid[band],chip)
             try:
                 bash("mv %s %s" % (out_from, out_to))
             except: # off chip
@@ -65,59 +75,69 @@ class PhoSimSet:
 
     def img(self,band):
         m0 = 15.0
-        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.set))
-        out_to = os.path.abspath(os.path.join(out_dir,"img_%s.fits.gz" % band))
         # setup catalog file
         with open("examples/maskrcnn_catalog_%s" % band,"w+") as f:
-            print((self.ra,self.dec,self.filterid[band],self.exptime[band],self.obsid[band],self.seed,m0,self.maglim,m0,self.maglim))
-            f.write("rightascension %f\ndeclination %f\nfilter %d\nvistime %f\nnsnap 1\nobshistid %d\nseed %d\nstars %f %f 0.1\ngalaxies %f %f 0.1" % (self.ra,self.dec,self.filterid[band],self.exptime[band],self.obsid[band],self.seed,m0,self.maglim,m0,self.maglim))
+            f.write("rightascension %f\ndeclination %f\nfilter %d\nvistime %f\nnsnap 1\nobshistid %d\nseed %d\nstars %f %f %f\ngalaxies %f %f %f" % (self.ra,self.dec,self.filterid[band],self.exptime[band],self.obsid[band],self.seed,m0,self.maglim,self.fov,m0,self.maglim,self.fov))
         # Run PhoSim
         bash("./phosim examples/maskrcnn_catalog_%s -c %s -i %s -t %d -e 0" % (band,COMMANDFILE_IMG,self.instrument,self.nproc//len(self.bands)))
         # Find chips in output and make set directories for each
-        bash("mv ./output/%s_e_%d_f%d_4S_E000.fits.gz %s" % (self.instrument,self.obsid[band],self.filterid[band],out_to))
+        fs = glob("./output/%s_e_%d_f%d_*_E000.fits.gz" % (self.instrument,self.obsid[band],self.filterid[band]))
+        chips = [fs.split('_')[-2] for f in fs]
+        sets = self.npointing+range(len(chips))
+        # Map chip name to set dir
+        self.sets = dict(zip(chips,sets))
+        # Set (chip) loop
+        for i,f in enumerate(fs):
+            # Move and rename final images in each band
+            out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.npointing+i))
+            out_to = os.path.abspath(os.path.join(out_dir,"img_%s.fits.gz" % band))
+            bash("mv %s %s" % (f,out_to))
         return
 
     def simulate(self):
-        out_dir = os.path.abspath(os.path.join(self.train_dir,"set_%d/" % self.set))
-        if os.path.exists(out_dir):
-            print("Error: Set %d already exists." % self.set)
-            return
-        os.mkdir(out_dir)
-        # Run PhoSim on chip S4 with typical stars and galaxies
+        # Clean work dir
+        print('Cleaning work directory.')
+        [os.remove(f) for f in glob(os.path.abspath("./work/*"))]
+        print('Simulating.')
+        # Run PhoSim pointing with typical stars and galaxies
         pool = ThreadPool(len(self.bands))
         out = pool.map(self.img, self.bands)
         pool.close()
         pool.join()
         # Read the trimmed catalog for chip S4 and run on each object to generate mask
-        with open("./work/trimcatalog_%d_4S.pars" % self.obsid[self.bands[0]],"r") as f:
-            lines = []
-            readlines = f.readlines()
-            for i,line in enumerate(readlines):
-                if len(line.strip()) != 0:
-                    obj_id = line.split()[1]
-                    if ".1" in obj_id: # 1st component of galaxy
-                        # add second component
-                        lines.append(line+"\n"+readlines[i+2])
-                    elif ".2" in obj_id:
-                        continue # already added 2nd component
-                    else: # star
-                        lines.append(line)
-            pool = ThreadPool(int(np.sqrt(self.nproc)))
-            out = pool.map(self.mask, lines)
-            pool.close()
-            pool.join()
+        fs = os.path.abspath("./work/trimcatalog_%d_*.pars" % self.obsid[self.bands[0]])
+        # Set (chip) loop
+        for f in fs:
+            chip = f.split('_')[-1].split('.pars')[0]
+            setnum = self.sets[chip]
+            os.mkdir(os.path.abspath(os.path.join(self.train_dir,"set_%d/" % setnum)))
+            with open(f,"r") as f:
+                lines = []
+                readlines = f.readlines()
+                for i,line in enumerate(readlines):
+                    if len(line.strip()) != 0:
+                        obj_id = line.split()[1]
+                        if ".1" in obj_id: # 1st component of galaxy
+                            # add second component
+                            lines.append(line+"\n"+readlines[i+2])
+                        elif ".2" in obj_id:
+                            continue # already added 2nd component
+                        else: # star
+                            lines.append(line)
+                # Simulate masks
+                pool = ThreadPool(int(np.sqrt(self.nproc)))
+                out = pool.starmap(self.mask,zip(lines,repeat(chip),repeat(setnum)))
+                pool.close()
+                pool.join()
 
 def combine_masks(out_dir):
     # combine masks into one multi-extension HDU
-    num_sets = 0
-    for setdir in os.listdir(out_dir):
-        if 'set_' in setdir:
-            num_sets += 1
-
-    for i in range(num_sets):
+    fs = glob(os.path.abspath(os.path.join(out_dir,'set_*')))
+    # Set loop
+    for i,f in enumerate(fs):
         hdul = fits.HDUList()
-        setdir = 'set_%d' % i
-        for image in os.listdir(os.path.join(out_dir,setdir)):
+        # mask image loop
+        for image in os.listdir(f):
             if (image.endswith('.fits.gz') or image.endswith('.fits')) and not 'img' in image:
                 image = os.path.join(out_dir,setdir,image)
                 data = getdata(image)
@@ -140,27 +160,32 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Simulate training set data using PhoSim.')
     parser.add_argument("instrument",type=str,help="Which PhoSim telescope/instrument to use.")
+    parser.add_argument("--fov",type=float,default=0.1,help="Field of view in degrees of catalog to generate, e.g. 0.01 to test.")
     parser.add_argument("--bands",type=str,default='grz',help="Which bands to use, e.g. 'grz' or 'grizY'.")
-    parser.add_argument("--exptimes",type=str,default='120,120,120',help="Exposure times to use in each band, e.g. '120,120,120'.")
-    parser.add_argument("--nset",type=int,default=1,help="Number of sets of images to simulate.")
+    parser.add_argument("--exptimes",type=str,default='120,120,120',help="Exposure times in seconds to use in each band, e.g. '120,120,120'.")
+    parser.add_argument("--npoint",type=int,default=1,help="Number of sets of images to simulate.")
     parser.add_argument("--maglim",type=float,default='23.0',help="Limiting magnitude.")
     parser.add_argument("--nproc",type=int,default=mp.cpu_count(),help="Number of processes.")
     args = parser.parse_args()
     exptimes = np.array(args.exptimes.split(","),dtype=float)
-
+    
     print('------------------------------------------------------------------------------------------')
     print('Simulate PhoSim Training Data')
     print('------------------------------------------------------------------------------------------')
+    print('Output directory: %s' % TRAIN_DIR)
     print('Instrument: %s' % args.instrument)
+    print('FoV: %1.2f' % args.fov)
     print('Bands: %s' % args.bands)
     print('Exp. Times: %s' % args.exptimes)
-    print('Number of sets: %d' % args.nset)
+    print('Number of pointings: %d' % args.npoint)
     print('Limiting mag: %2.1f' % args.maglim)
-    print('Noumber of processes: %d' % args.nproc)
+    print('Number of processes: %d' % args.nproc)
     print('------------------------------------------------------------------------------------------')
 
-    # set loop
-    for i in range(args.nset):
-        s = PhoSimSet(i,args.instrument,args.bands,exptimes,args.maglim,TRAIN_DIR,args.nproc)
+    # WARNING: Be sure to remove lines 740-742 in phosim's phosim.py to keep the trimcatalog file from being removed!
+
+    # Pointings loop
+    for i in range(args.npoint):
+        s = PhoSimSet(i,args.instrument,args.fov,args.bands,exptimes,args.maglim,TRAIN_DIR,args.nproc)
         s.simulate()
     combine_masks(TRAIN_DIR)
